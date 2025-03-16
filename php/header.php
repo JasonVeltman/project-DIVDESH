a17e220e8ac5c471f50a0edfc97304605a2e4f82
JasonVeltman <jasonveltman4@gmail.com>
2025-03-16 15:09:13 +0100

    begin

---
+++ php/header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <img src="img/logo_groot.png" alt="logo">
+        <a href="index.html"></a>
+        <a href="lunch_diner.html"></a>
+        <a href="openingstijden_locatie.html"></a>
+        <a href="reserveren.html"></a>
+        <a href="vactures.html"></a>
+    </nav>
+</header>
