003520e6bf9ab1b397c79e571742a063c53a046f
JasonVeltman <jasonveltman4@gmail.com>
2025-03-17 15:56:11 +0100

    kleine kleine fix

--- php/header.php
+++ php/header.php
@@ -3,8 +3,8 @@
         <img src="img/logo_groot.png" alt="logo" class="Glogo">
         <img src="img/logo_klein.png" alt="logo" class="Klogo">
         <a href="index.php" class="Hlinks">Home</a>
-        <a href="lunch_diner.php" class="Hlinks">Lunch_Diner</a>
-        <a href="openingstijden_locatie.php" class="Hlinks">Openingstijden_Locatie</a>
+        <a href="lunch_diner.php" class="Hlinks">Lunch/Diner</a>
+        <a href="openingstijden_locatie.php" class="Hlinks">Openingstijden/Locatie</a>
         <a href="reserveren.php" class="Hlinks">Reserveren</a>
         <a href="vactures.php" class="Hlinks">Vactures</a>
     </nav>
