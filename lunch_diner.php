99ce9e7d6fb8e53cfefa5d21425d5656fdf81d6c
zowipty <joshroelse@gmail.com>
2025-03-24 14:14:19 +0100

    deze gebruiken

--- lunch_diner.php
+++ lunch_diner.php
@@ -8,24 +8,33 @@
     <meta name="author" content="Josh Roelse, Mohamed Mussa">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lunch_Diner</title>
-    <link rel="stylesheet" href="css/Josh.css"> 
+    <link rel="stylesheet" href="css/Josh.css">
 </head>
 
 <body>
 
-    <?php require 'HF/header.php'; ?> 
+    <?php require 'HF/header.php'; ?>
 
-<section>
-    <article>
-        <img src="img/Lunch_diner.png" alt="banner" class="bannerLunchDiner">
-    </article>
-</section>
+    <section>
+        <article>
+            <img src="img/Lunch_diner.png" alt="banner" class="bannerLunchDiner">
+        </article>
+    </section>
 
-
-<h1>Lunch</h1>
-<p>Crispy sushi bites - 12</p>
-<p>Maki - 12</p>
-<p>Crispy sushi - 12</p>
+    <section>
+        <article>
+            <h1>Lunch</h1>
+            <p>Crispy sushi bites - 12</p>
+            <p>Maki - 12</p>
+            <p>Crispy sushi - 12</p>
+        </article>
+        <article>
+            <h1>Diner</h1>
+            <p>Rijstbowl - 12</p>
+            <p>Sushi Rijst - 12</p>
+            <p>Onigiri - 12</p>
+        </article>
+    </section>
 
     <?php require 'HF/footer.php'; ?>
 
