2a9a45d08fdf0e2c4ab3df4f824c47f6b676e835
JasonVeltman <jasonveltman4@gmail.com>
2025-03-25 10:22:26 +0100

    dinsdag les gedaan

--- HF/header.php
+++ HF/header.php
@@ -10,21 +10,14 @@
     <nav>
         <img src="img/logo_groot.png" alt="logo" class="Glogo">
         <img src="img/logo_klein.png" alt="logo" class="Klogo">
-        <button class="hamburger-menu" onclick="toggleMenu()">☰</button>
-        <section class="nav-links">
+            <button class="hamburger-menu" onclick="toggleMenu()">☰</button>
             <a href="index.php" class="Hlinks">Home</a>
             <a href="lunch_diner.php" class="Hlinks">Lunch/Diner</a>
             <a href="openingstijden_locatie.php" class="Hlinks">Openingstijden/Locatie</a>
             <a href="reserveren.php" class="Hlinks">Reserveren</a>
             <a href="vactures.php" class="Hlinks">Vactures</a>
-        </section>
+
     </nav>
 </header>
-<script>
-    function toggleMenu() {
-        const navLinks = document.querySelector('.nav-links');
-        navLinks.classList.toggle('active');
-    }
-</script>
 </body>
 </html>
