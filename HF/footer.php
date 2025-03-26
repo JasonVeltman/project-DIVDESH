c84102e6368bdb87ee863162187444d84a475b7f
JasonVeltman <jasonveltman4@gmail.com>
2025-03-26 13:26:39 +0100

    woensdag tussentijdse push

--- HF/footer.php
+++ HF/footer.php
@@ -1,16 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
 <footer>
 
-    <section>
-        <article class="social-icons">
-            <a href="https://facebook.com" target="_blank">
-                <img src="img/Facebook.png" alt="Facebook">
-            </a>
-            <a href="https://instagram.com" target="_blank">
-                <img src="img/Instagram.png" alt="Instagram">
-            </a>
-            <a href="https://twitter.com" target="_blank">
-                <img src="img/X.png" alt="X">
-            </a>
-        </article>
-    </section>
+<section>
+    <article class="social-icons">
+        <a href="https://facebook.com" target="_blank">
+            <img src="img/Facebook.png" alt="Facebook">
+        </a>
+        <a href="https://instagram.com" target="_blank">
+            <img src="img/Instagram.png" alt="Instagram">
+        </a>
+        <a href="https://twitter.com" target="_blank">
+            <img src="img/x.png" alt="X">
+        </a>
+    </article>
+</section>
 </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,30 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="Jason Veltman, Mohamed Mussa">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Umami Sushi</title>
     <link rel="stylesheet" href="css/style.css">
+
 </head>
 
 <body>
 
     <?php require 'HF/header.php'; ?>
+    <main>
 
-    <section class="text">
-         Umami Sushi is een
-        sfeervol sushi-restaurant met
-        verse, smaakvolle
-        Japanse gerechten. Geniet
-        van diverse sushirollen, sashimi
-        en warme specialiteiten in
-        een moderne en gastvrije ambiance. </>
-        <button class="home-button"><a href="reseer.php">Reserveer Hier</a></button>
-    </section>
+        <section class="main-img">
+            <img src="img/home_sushi.png" alt="Sushi Image" class="begin_img">
+        </section>
+
+        <section class="content">
+            <p>Umami Sushi is een sfeervol sushi-restaurant met verse, smaakvolle Japanse gerechten. Geniet van diverse
+                sushirollen, sashimi en warme specialiteiten in een moderne en gastvrije ambiance.</p>
+            <a href="reserveren.php" class="button">reserveren</a>
+        </section>
+
+    </main>
+    <?php require 'HF/footer.php'; ?>
 
 </body>
 
