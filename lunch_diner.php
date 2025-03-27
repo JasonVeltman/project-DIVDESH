376742860c60cce80674b539ae608ae9a26b98ce
zowipty <joshroelse@gmail.com>
2025-03-27 20:57:19 +0100

    klaar (voor nu denk ik)

--- lunch_diner.php
+++ lunch_diner.php
@@ -9,26 +9,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lunch_Diner</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="javacript/script.js" defer></script>
 </head>
 
 <body>
 
     <?php require 'HF/header.php'; ?>
 
-    <section>
+    <section class="grid-container">
         <article>
             <img src="img/Lunch_diner.png" alt="banner" class="bannerLunchDiner">
-        </article>
-    </section>
-
-    <section>
-        <article>
             <h1>Lunch</h1>
             <p>Crispy sushi bites - 12</p>
             <p>Maki - 12</p>
             <p>Crispy sushi - 12</p>
         </article>
         <article>
+            <img src="img/Onigiri.png" alt="banner" class="sushiOnigiri">
             <h1>Diner</h1>
             <p>Rijstbowl - 12</p>
             <p>Sushi Rijst - 12</p>
--- openingstijden_locatie.php
+++ openingstijden_locatie.php
@@ -8,15 +8,48 @@
     <meta name="author" content="Josh Roelse">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="css/josh.css"> 
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
-    <?php require 'HF/header.php'; ?> 
+    <?php require 'HF/header.php'; ?>
 
 
-   
+<section><article>
+    <img src="img/sushibord.png" alt="banner" class="bannerOpeningstijden">
+</article></section>
+
+    <section class="openingstijden">
+        <article>
+            <h1>Openingstijden</h1>
+            <p>Maandag: 15:00 - 21:00</p>
+            <p>Dinsdag: 15:00 - 21:00</p>
+            <p>Woensdag: 15:00 - 21:00</p>
+            <p>Donderdag: 15:00 - 21:00</p>
+            <p>Vrijdag: 15:00 - 21:0</p>
+            <p>Zaterdag: 15:00 - 21:00</p>
+            <p>Zondag: Gesloten</p>
+        </article>
+    </section>
+
+    <section class="locatie">
+        <article>
+            <h1>Locatie</h1>
+            <p>Adresgegevens Umani Sushi</p>
+            <p>Galgewater 12</p>
+            <p>2311VZ Leiden</p>
+            <p>email: info@umamileiden.nl</p>
+            <p>Telefoonnummer: 088-2324456</p>
+        </article>
+    </section>
+
+    <section class="locatieKaart">
+        <article>
+            <h1>Locatie Kaart</h1>
+           <a href="https://maps.google.com/maps"><img src="img/map.png" alt="kaart" class="kaart"></a>
+        </article>
+    <?php require 'HF/footer.php'; ?>
 
 </body>
 
