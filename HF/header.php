873989941a7cbf483ca6d9e7bfae14e91b820da5
JasonVeltman <jasonveltman4@gmail.com>
2025-03-27 12:17:31 +0100

    donderdag

--- HF/header.php
+++ HF/header.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
-<body>
-<header>
-    <nav>
-        <img src="img/logo_groot.png" alt="logo" class="Glogo">
-        <img src="img/logo_klein.png" alt="logo" class="Klogo">
-            <button class="hamburger-menu" onclick="toggleMenu()">☰</button>
-            <a href="index.php" class="Hlinks">Home</a>
-            <a href="lunch_diner.php" class="Hlinks">Lunch/Diner</a>
-            <a href="openingstijden_locatie.php" class="Hlinks">Openingstijden/Locatie</a>
-            <a href="reserveren.php" class="Hlinks">Reserveren</a>
-            <a href="vactures.php" class="Hlinks">Vactures</a>
 
-    </nav>
-</header>
+<body>
+    <header>
+        <nav>
+            <img src="img/logo_groot.png" alt="logo" class="Glogo">
+            <img src="img/logo_klein.png" alt="logo" class="Klogo">
+            <button class="hamburger-menu" onclick="toggleMenu()">☰</button>
+            <section>
+                <a href="index.php" class="Hlinks">Home</a>
+                <a href="lunch_diner.php" class="Hlinks">Lunch/Diner</a>
+                <a href="openingstijden_locatie.php" class="Hlinks">Openingstijden/Locatie</a>
+                <a href="reserveren.php" class="Hlinks">Reserveren</a>
+                <a href="vactures.php" class="Hlinks">Vactures</a>
+            </section>
+        </nav>
+    </header>
 </body>
+
 </html>
