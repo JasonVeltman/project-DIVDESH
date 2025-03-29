273062505a3def103ea7945783b0a6ce43582d1f
JasonVeltman <jasonveltman4@gmail.com>
2025-03-29 13:18:01 +0100

    2de pagina af
    alleen nog headers fixen op mijn paginas dan ben ik klaar

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Umami Sushi - De beste sushi in Leiden. Geniet van onze heerlijke gerechten en reserveer vandaag nog!">
+    <meta name="description"
+        content="Umami Sushi - De beste sushi in Leiden. Geniet van onze heerlijke gerechten en reserveer vandaag nog!">
     <meta name="keywords" content="Umami Sushi, sushi, restaurant, Leiden, reserveren, Japanse gerechten">
     <meta name="author" content="Jason Veltman">
     <title>Umami Sushi</title>
@@ -20,12 +21,13 @@
         <section class="main-img">
             <img src="img/home_sushi.png" alt="Sushi Image" class="begin_img">
         </section>
-
+        <br><br>
         <section class="content">
-            <p>Umami Sushi is een sfeervol sushi-restaurant met verse, <br>smaakvolle Japanse gerechten. Geniet van diverse
+            <p>Umami Sushi is een sfeervol sushi-restaurant met verse, <br>smaakvolle Japanse gerechten. Geniet van
+                diverse
                 sushirollen, sashimi en warme specialiteiten in een moderne<br> en gastvrije ambiance.</p>
         </section>
-        
+
         <a href="reserveren.php" class="button">reserveren</a>
 
     </main>
--- reserveren.php
+++ reserveren.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 
 <head>
     <meta charset="UTF-8">
@@ -8,12 +8,49 @@
     <meta name="author" content="Jason Veltman">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
-    <?php require 'php/header.php'; ?> 
+    <?php require 'HF/header.php'; ?>
+    <main>
+
+        <section class="main-img">
+            <img src="img/home_sushi.png" alt="Sushi Image" class="begin_img">
+        </section>
+        <form class="content">
+            <label for="L/D">Lunch/Diner</label><br>
+            <select name="L/D" class="options">
+                <option value="Lunch">Lunch</option>
+                <option value="Diner">Diner</option>
+            </select>
+
+            <br><br><br><br>
+
+            <label for="aantal-p">Hoeveel personen?</label><br>
+            <input type="text" class="options">
+
+            <br><br><br><br>
+
+            <label for="Dag">Welke dag?</label><br>
+            <select name="L/D" class="options">
+                <option value="30/3/2025">30/3/2025</option>
+                <option value="31/3/2025">31/3/2025</option>
+                <option value="32/3/2025">32/3/2025</option>
+                <option value="33/3/2025">33/3/2025</option>
+                <option value="34/3/2025">34/3/2025</option>
+                <option value="35/3/2025">35/3/2025</option>
+                <option value="36/3/2025">36/3/2025</option>
+            </select>
+        </form>
+
+        <br><br>
+
+        <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0&t=2s" class="button">reserveren</a>
+
+    </main>
+    <?php require 'HF/footer.php'; ?>
 
 </body>
 
